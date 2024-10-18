91d282e02ab765cafb4ab51a3558714278397667
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2024-10-18 13:06:26 +0200

    add fitness function for clean

---
+++ ebike-clean/src/test/java/ass01/core/FitnessFunctionTest.java
@@ -0,0 +1,21 @@
+package ass01.core;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+
+import org.junit.jupiter.api.Test;
+public class FitnessFunctionTest {
+
+    private final JavaClasses importedClasses = new ClassFileImporter().importPackages("ass01.core");
+    @Test
+    public void businessDependencyRule() {
+        ArchRule rule =
+                noClasses().that().resideInAPackage("..business..")
+                        .should().dependOnClassesThat().resideInAPackage("..adapters..");
+
+        rule.check(importedClasses);
+    }
+}
